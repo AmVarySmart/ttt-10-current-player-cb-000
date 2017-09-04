a1896a1564798ce317599013a16819a69a642b02
Tyriek Barker <tyriekbarker@gmail.com>
2017-09-04 17:07:49 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,12 @@
+def turn_count(board)
+  turns = 0
+  board.each do |space|
+    space == "X" || space == "O" ? turns += 1 : true
+  end
+  turns
+end
+
+def current_player(board)
+  turns = turn_count(board)
+  turns.even? ? "X" : "O"
+end
